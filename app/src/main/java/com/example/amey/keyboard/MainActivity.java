7483bf6e77c3cdf1067025252893b5225cd845de
ameybhivshet <ameybhivshet26995@gmail.com>
2016-09-24 12:43:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/amey/keyboard/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.amey.keyboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements  TextView.OnEditorActionListener {
+EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText= (EditText) findViewById(R.id.editText);
+        editText.setOnEditorActionListener(this);
+    }
+
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+        if(actionId== EditorInfo.IME_ACTION_DONE)
+        {
+            Toast.makeText(MainActivity.this, "done", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return false;
+    }
+}
